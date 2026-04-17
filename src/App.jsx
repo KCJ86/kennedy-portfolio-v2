f7d6bc3e81ed8c743628c0882d25cad5a8564c3b
Kennedy Castillon Jimenez <kimenez29@gmail.com>
2026-04-16 22:35:44 -0700

    feat(analytics): added some vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Navbar }     from "./components/Navbar/Navbar";
-import { Hero }       from "./components/Hero/Hero";
-import { Projects }   from "./components/Projects/Projects";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
+import { Projects } from "./components/Projects/Projects";
 import { Experience } from "./components/Experience/Experience";
-import { About }      from "./components/About/About";
-import { Contact }    from "./components/Contact/Contact";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <main>
         <Hero />
